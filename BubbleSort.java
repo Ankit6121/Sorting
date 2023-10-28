a1a0c4a57ec85964dd1d9741766357db41014cfa
Ankit6121 <146847594+Ankit6121@users.noreply.github.com>
2023-10-29 01:18:10 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+// it is working properly
+public class BubbleSort {
+    public static void bubblesort(int arr[]) {
+        int n = arr.length;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of the array!");
+        int n = sc.nextInt();
+        System.out.println("enter the element of the array");
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        bubblesort(arr);
+        System.out.println("after sorting array is:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ ShellSort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+// it is working properly 
+
+public class ShellSort {
+    public static void shellsort(int arr[]) {
+        int n = arr.length;
+        int gap = n / 2;
+        int temp, i, j;
+        for (gap = n / 2; gap > 0; gap = gap / 2) {
+            for (i = gap; i < n; i++) {
+                temp = arr[i];
+                for (j = i; j >= gap && arr[j - gap] > temp; j = j - gap) {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of the array!");
+        int n = sc.nextInt();
+        System.out.println("enter the element of the array");
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        shellsort(arr);
+        System.out.println("after sorting array is:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
